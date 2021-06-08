e196f8d68ac9daa2e3255f99c4e07493aee7d1e4
wtface <nkhpark@hotmail.com>
2021-06-08 09:51:19 +0800

    feat: img route showing an image

--- dist/index.js
+++ dist/index.js
@@ -8,9 +8,19 @@ var logger_1 = __importDefault(require("./utilities/logger"));
 var app = express_1.default();
 var router = express_1.default.Router();
 var port = 3000;
-router.use('/user', logger_1.default, function (req, res, next) {
+app.use(express_1.default.static('public'));
+router.use('/img', logger_1.default, function (req, res, next) {
     next();
-    res.send('user page');
+    // const root = path.join(__dirname, '../public');
+    var name = req.query.filename;
+    var width = req.query.width;
+    var height = req.query.height;
+    if (!name) {
+        res.send('need to pass filename');
+    }
+    var html = "<!DOCTYPE html>\n    <html lang=\"en\">\n    <head>\n        <meta charset=\"UTF-8\">\n    </head>\n    <body>\n        <img src=\"original/" + name + ".jpeg\" alt=\"\" width=\"" + width + "\" height=\"" + height + "\">\n    </body>\n    </html>";
+    res.set('Content-Type', 'text/html');
+    res.send(html);
 });
 app.use('/', router);
 app.listen(port, function () {
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,34 @@
 import express from 'express';
 import endpointLogger from './utilities/logger';
-var app = express();
-var router = express.Router();
-const port = 3000;
+import path from 'path';
 
-router.use('/user', endpointLogger, function (req, res, next) {
+const app = express();
+const router = express.Router();
+const port = 3000;
+app.use(express.static('public'));
+
+router.use('/img', endpointLogger, function (req, res, next) {
     next();
-    res.send('user page');
+    // const root = path.join(__dirname, '../public');
+    const name = req.query.filename;
+    const width = req.query.width;
+    const height = req.query.height;
+
+    if (!name) {
+        res.send('need to pass filename');
+    }
+
+    let html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <img src="original/${name}.jpeg" alt="" width="${width}" height="${height}">
+    </body>
+    </html>`;
+    res.set('Content-Type', 'text/html');
+    res.send(html);
 });
 
 app.use('/', router);
