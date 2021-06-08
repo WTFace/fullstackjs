acb5b339b36e4680e6e7a5d2ec05732d0f1fe30e
wtface <nkhpark@hotmail.com>
2021-06-08 11:01:18 +0800

    chore: ignore dist

--- dist/utilities/logger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var endpointLogger = function (req, res, next) {
-    console.log(req.originalUrl);
-    next();
-};
-exports.default = endpointLogger;
