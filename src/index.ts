06952bbd6bc09eb4a6cbd8c8d810c781ee38ed0b
wtface <nkhpark@hotmail.com>
2021-06-09 19:40:55 +0800

    fix: function parameters and return type

--- src/index.ts
+++ src/index.ts
@@ -7,28 +7,40 @@ const router = express.Router();
 const port = 3000;
 app.use(express.static('public'));
 
-router.use('/img', fileCheck, async function (req, res, next) {
-    next();
-    const name = req.query.filename;
-    const width = req.query.width;
-    const height = req.query.height;
+router.use(
+    '/img',
+    fileCheck,
+    async function (
+        req: express.Request,
+        res: express.Response,
+        next: Function
+    ): Promise<void> {
+        next();
+        const name = req.query.filename;
+        const width = req.query.width;
+        const height = req.query.height;
 
-    await processImg(name as string, +(width as string), +(height as string))
-        .then((filePath) => {
-            const html = `<!DOCTYPE html>
+        await processImg(
+            name as string,
+            +(width as string),
+            +(height as string)
+        )
+            .then((filePath) => {
+                const html = `<!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
         </head>
         <body><img src="${filePath.split('public/')[1]}" alt=""></body>
         </html>`;
-            res.set('Content-Type', 'text/html');
-            return res.send(html);
-        })
-        .catch((e) => {
-            console.log(e);
-        });
-});
+                res.set('Content-Type', 'text/html');
+                return res.send(html);
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    }
+);
 
 app.use('/', router);
 
