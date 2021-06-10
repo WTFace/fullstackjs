54925c9cea18922811f8e57ede9b7a116051ac9e
wtface <nkhpark@hotmail.com>
2021-06-10 13:31:54 +0800

    await sharp

--- src/index.ts
+++ src/index.ts
@@ -24,8 +24,11 @@ router.use(
             name as string,
             +(width as string),
             +(height as string)
-        ).then((filePath) => {
-            const html = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"></head><body><img src="${filePath.split('public/')[1]}" alt=""></body></html>`;
+        )
+            .then((filePath) => {
+                const html = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"></head><body><img src="${
+                    filePath.split('public/')[1]
+                }" alt=""></body></html>`;
                 res.set('Content-Type', 'text/html');
                 return res.send(html);
             })
--- src/utilities/resize.ts
+++ src/utilities/resize.ts
@@ -12,20 +12,20 @@ const processImg = async (
             reject('wrong dimensions');
         } else {
             // check if the resized file exists
-            access(outFile, (err) => {
+            access(outFile, async (err) => {
                 if (err) {
                     if (isNaN(+w) || isNaN(+h)) {
                         reject('wrong dimensions');
                     } else {
                         // resize and save if the dimension doesn't exist
-                        sharp(`public/original/${name}.jpeg`)
+                        await sharp(`public/original/${name}.jpeg`)
                             .resize(+w, +h)
                             .toFile(outFile, function (err) {
                                 if (err) {
                                     reject(err);
                                 }
-                                resolve(outFile);
                             });
+                        resolve(outFile);
                     }
                 } else {
                     resolve(outFile);
